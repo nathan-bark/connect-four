6e06ddda09db9b74c87ae95cec8f9c332415f1f7
Nathan <nathanbark4@gmail.com>
2024-09-30 14:51:28 +0100

    Finish main menu styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -8,7 +7,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainMenu />} />
+        <Route path="/" element={<MainMenu />} >
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
